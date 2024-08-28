d496b1281fa6abb9be78f94e985ecd521115b1c0
Ksenia <ksenia.briling@epfl.ch>
2024-08-28 17:25:26 +0200

    Update sweep wrt 87210f357

--- sweep.py
+++ sweep.py
@@ -28,6 +28,7 @@ parser.add_argument('-d', '--dataset', default='yuri', help='dataset')
 parser.add_argument('-t', '--target', default='gap', help='target column')
 parser.add_argument('--id', default=None, help='sweep id if continue a sweep')
 parser.add_argument('--equivariant', action='store_true', help='use equivariant model (default: invariant)')
+parser.add_argument('--local', action='store_true', help='use local (masked) model')
 script_args = parser.parse_args()
 
 features = {'yuri': 'torchchem_v1'}
@@ -55,7 +56,6 @@ sweep_config = { 'method': 'bayes', 'metric': metric }
 parameters_dict = {
     'distance_emb_dim': { 'values': [16, 32, 48, 64] },
     'dropout_p': { 'values': [0.0, 0.05, 0.1] },
-    'graph_mode': { 'values': ['energy', 'vector'] },
     'max_neighbors': { 'values': [10, 25, 50] },
     'n_conv_layers': { 'values': [2, 3] },
     'n_s': { 'values': [16, 32, 48, 64] },
@@ -64,6 +64,12 @@ parameters_dict = {
     'lr':  { 'values' : [0.00005, 0.0001, 0.0005, 0.001] },
     'weight_decay' : { 'values' : [1e-5, 1e-4, 1e-3, 0] },
     }
+
+
+if script_args.local:
+    parameters_dict.update({'graph_mode': { 'value': 'vector_masked' }})
+else:
+    parameters_dict.update({'graph_mode': { 'values': ['energy', 'vector'] }})
 
 if script_args.equivariant:
     parameters_dict.update({'n_v': { 'values': [16, 32, 48, 64] }})
