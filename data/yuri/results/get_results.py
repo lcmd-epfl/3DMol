579ceb618563ecf6415616c0b5bc4a8ee7fb6627
Ksenia <ksenia.briling@epfl.ch>
2024-08-29 22:41:44 +0200

    Yuri dataset results

---
+++ data/yuri/results/get_results.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import os.path
+import pandas as pd
+
+results_path = 'results.csv'
+raw_data_path = 'project_raw.csv'
+raw_data_path = f'{os.path.dirname(__file__)}/../hypers/{raw_data_path}'
+
+if not os.path.isfile(raw_data_path):
+    raise RuntimeError(f'Result file {raw_data_path} is missing')
+
+df = pd.read_csv(raw_data_path)
+df = df[pd.isna(df['sweep_id'])]
+
+results = []
+for name in set(df.name):
+    d = df[df['name'] == name].reset_index(drop=True)
+    assert len(d) == 10
+    test_score = d.test_score
+    results.append({
+        'name': name,
+        'target':    d.target_column[0],
+        'scope':     'local' if d.graph_mode[0]=='vector_masked' else 'global',
+        'mae_mean':  d.test_score.mean(),
+        'mae_std':   d.test_score.std(),
+        'rmse_mean': d.test_rmse.mean(),
+        'rmse_std':  d.test_rmse.std()
+        })
+
+results = pd.DataFrame(results)
+results.sort_values(by=['target', 'scope'], inplace=True, ignore_index=True)
+results.to_csv(results_path, index=False)
