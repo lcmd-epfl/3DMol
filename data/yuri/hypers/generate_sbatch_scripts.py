cd377d7bc070b3054bfce7d25a5adca6547518c8
Ksenia <ksenia.briling@epfl.ch>
2024-08-29 18:21:30 +0200

    Add sweep results

--- data/yuri/hypers/generate_sbatch_scripts.py
+++ data/yuri/hypers/generate_sbatch_scripts.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 
+from glob import glob
 import numpy as np
 
-for y in ['gap', 'HOMO', 'N_FOD', 'dipole', 'hirshfeld']: #, 'LUMO', 'splitting']:
+for config in glob('config-*-*-????????.dat'):
 
-    base_config = np.loadtxt(f'{y}-config.dat', skiprows=1, dtype=str)
+    base_config = np.loadtxt(config, skiprows=1, dtype=str)
     base_config = {key: val.strip('"') for key, val in base_config}
     config = '\n'.join([f'--{key} {val if val!="true" else ""} \\' for key, val in base_config.items()])
 
     short_name = f"{base_config['dataset']}-{base_config['target_column']}-{'inv' if base_config['invariant']=='true' else 'equiv'}-noH"
-    full_name = f"{short_name}-ns{base_config['n_s']}-nv{base_config['n_v']}-d{base_config['distance_emb_dim']}-l{base_config['n_conv_layers']}-{base_config['graph_mode']}-{base_config['sum_mode']}"
+    full_name = f"{short_name}-ns{base_config['n_s']}-{'nv'+base_config['n_v']+'-' if base_config['n_v']!='nan' else ''}d{base_config['distance_emb_dim']}-l{base_config['n_conv_layers']}-{base_config['graph_mode']}-{base_config['sum_mode']}"
 
     header=f"""#!/bin/bash -l
 #SBATCH --partition=gpu
@@ -19,7 +20,7 @@ for y in ['gap', 'HOMO', 'N_FOD', 'dipole', 'hirshfeld']: #, 'LUMO', 'splitting'
 #SBATCH --ntasks=1
 #SBATCH --mem=4GB
 #SBATCH --time=1:00:00
-#SBATCH --job-name=3dmol
+#SBATCH --job-name={base_config['target_column']}-{base_config['graph_mode']}
 #SBATCH --array=0-9
 #SBATCH --exclude=i39
 
@@ -39,8 +40,8 @@ python train.py \\"""
 --splitter test:data/yuri/splits/fold_${{FOLD}}_test_indices.npy \\
 --logdir /scratch/izar/briling/cv \\
 --print_predictions \\
---wandb_name cv-{full_name} \\"""
+--wandb_name cv10-{full_name} \\"""
 
-    with open(f'{short_name}.sbatch', 'w') as f:
+    with open(f'{full_name}.sbatch', 'w') as f:
         print("\n".join((header, run_config, config)), file=f)
         print(file=f)
