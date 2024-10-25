8b4b694a8455b1b6f23c3658b7aecc5b64f2c77d
Ksenia <ksenia.briling@epfl.ch>
2024-10-25 17:52:24 +0200

    Remove heavy and rare elements

--- data/rotation/1_clean_reaxys_data.py
+++ data/rotation/1_clean_reaxys_data.py
@@ -9,6 +9,13 @@ def canonicalize(x):
     except:
         return None
 
+
+def check_elements(x):
+    heavy_elements = ['I']
+    rare_elements = ['P', 'Se']
+    return len(set(a.GetSymbol() for a in Chem.MolFromSmiles(x).GetAtoms()).intersection(heavy_elements+rare_elements))==0
+
+
 df = pd.read_table('data_0_raw.tsv')
 df.dropna(subset='SMILES', inplace=True, ignore_index=True)
 df = df[df['SMILES'].str.contains('@')].reset_index(drop=True)
@@ -17,6 +24,7 @@ df.drop_duplicates('SMILES', keep='first', inplace=True, ignore_index=True)
 df['canon_SMILES'] = [*map(canonicalize, df.SMILES)]
 df.dropna(subset='canon_SMILES', inplace=True, ignore_index=True)
 df.drop_duplicates('canon_SMILES', keep='first', inplace=True, ignore_index=True)
+df = df[df.apply(lambda x: check_elements(x['canon_SMILES']), axis=1)]
 
 df_new = pd.DataFrame({'id':df['Substance Identification: Reaxys Registry Number'], 'SMILES':df['SMILES'], 'canon_SMILES':df['canon_SMILES'], 'specific_rotation': df['Optical Rotatory Power [deg]']})
 df_new.to_csv('data_1_cleaned.csv', index=False)
