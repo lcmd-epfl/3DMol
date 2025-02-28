afc4752f88a21231c5e98aea8cbf7b253d1b75d5
Ksenia <ksenia.briling@epfl.ch>
2025-02-28 03:33:51 +0100

    Add subsets for Yuri

---
+++ data/yuri/subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import pandas as pd
+
+path = 'refcode_properties_2300.csv'
+n = 5
+
+df = pd.read_csv(path)
+df.drop(labels=df.columns.difference(['refcode','gap']), axis=1, inplace=True)
+
+for i in range(n):
+    idx = np.loadtxt(f'refcode_subs/total_sub_{i}.txt', dtype=str)
+    df1 = df[df['refcode'].isin(idx)].reset_index(drop=True)
+    assert len(df1)==len(idx)
+    df1.to_csv(f'refcode_gap_{len(idx)}_{i}.csv', index=False)
