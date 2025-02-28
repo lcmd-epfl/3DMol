3fe6c28a5e9a49807b71df024b36b8027bc9ffcf
Ksenia <ksenia.briling@epfl.ch>
2025-02-28 04:10:12 +0100

    Add csv_path extra argument to some datasets

--- data/kulik/dataloader_kulik.py
+++ data/kulik/dataloader_kulik.py
@@ -8,14 +8,16 @@ class tmPHOTO(MolDataset):
                  target_column=None, geometry=None, graph_method=None,
                  noH=True, check=False):
 
-        self.version = 1  # INCREASE IF CHANGE THE DATA / DATALOADER / GRAPHS / ETC
-        self.csv_path='data/kulik/tmPHOTO_property.csv'
+        self.version = 2  # INCREASE IF CHANGE THE DATA / DATALOADER / GRAPHS / ETC
         self.processed_dir='data/kulik/processed/'
         self.smiles_column = None
         self.id_column = 'refcode'
-        self.default_parameters = dict(geometry='xtb', target_column='gap', graph_method='torchchem_v1')
+        self.default_parameters = dict(geometry='xtb', target_column='gap', graph_method='torchchem_v1',
+                                       _dl_extra_csv_path='data/kulik/tmPHOTO_property.csv')
         bad_indices = None
-        self.parameters = self.get_parameters(locals())
+        self.parameters = self.get_parameters(locals() | (extra_args if extra_args else {}))
+
+        self.csv_path = self.parameters._dl_extra_csv_path
 
         if self.parameters.geometry=='xtb':
             self.get_xyz_path = lambda idx: f'data/kulik/tmPHOTO-xyz/{idx}.xyz'
--- process/dataloader_yuri.py
+++ process/dataloader_yuri.py
@@ -8,14 +8,16 @@ class Yuri(MolDataset):
                  target_column=None, geometry=None, graph_method=None,
                  noH=True, check=False):
 
-        self.version = 3  # INCREASE IF CHANGE THE DATA / DATALOADER / GRAPHS / ETC
-        self.csv_path='data/yuri/refcode_properties_2300.csv'
+        self.version = 4  # INCREASE IF CHANGE THE DATA / DATALOADER / GRAPHS / ETC
         self.processed_dir='data/yuri/processed/'
         self.smiles_column = None
         self.id_column = 'refcode'
-        self.default_parameters = dict(geometry='default', target_column='gap', graph_method='torchchem_v1')
+        self.default_parameters = dict(geometry='default', target_column='gap', graph_method='torchchem_v1',
+                                       _dl_extra_csv_path='data/yuri/refcode_properties_2300.csv')
         bad_indices = None
-        self.parameters = self.get_parameters(locals())
+        self.parameters = self.get_parameters(locals() | (extra_args if extra_args else {}))
+
+        self.csv_path = self.parameters._dl_extra_csv_path
 
         if self.parameters.geometry=='default':
             self.get_xyz_path = lambda idx: f'data/yuri/0-XYZS/{idx}.xyz'
