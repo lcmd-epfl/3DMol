8a79919c3a85aab88d6d4cc665164013f1be3048
Ksenia <ksenia.briling@epfl.ch>
2025-05-28 22:55:54 +0200

    Use only positive specific rotation

--- data/rotation/10_bad_indices.py
+++ data/rotation/10_bad_indices.py
@@ -6,5 +6,8 @@ df = pd.read_csv(csv)
 
 negative_triple = df[df.triple_product<0].id.to_numpy()
 assert len(negative_triple)==len(df)//2
-
 np.savetxt('negative_triple_idx.dat', negative_triple, fmt='%s')
+
+negative_src = df[df.specific_rotation_computed<0].id.to_numpy()
+assert len(negative_src)==len(df)//2
+np.savetxt('negative_specific_rotation_computed_idx.dat', negative_src, fmt='%s')
