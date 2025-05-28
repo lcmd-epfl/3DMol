ec5f76a1de0639e06f967142386a80778c153638
Ksenia <ksenia.briling@epfl.ch>
2025-05-28 22:23:15 +0200

    Add geometric invariant

---
+++ data/rotation/9_triple.py
@@ -0,0 +1,42 @@
+from glob import glob
+import os
+import numpy as np
+import pandas as pd
+import ase.io
+from tqdm import tqdm
+
+
+def signed_volume(A, B, C, D):
+    return np.dot(B-A, np.cross(C-A, D-A))
+
+
+def compute_invariant(mol):
+    moments, axes = mol.get_moments_of_inertia(vectors=True)
+    idx = np.argsort(moments)[::-1]
+    moments = moments[idx]
+    axes    = axes   [idx]
+
+    r = (mol.get_positions()-mol.get_center_of_mass()) @ axes.T
+    #mol.set_positions(r)
+    #mol.write('molecule_rotated.xyz')
+
+    ix = np.argsort(abs(r[:,0]))[-1]
+    rx = r[ix]
+    r = r[np.r_[0:ix,ix+1:len(r)]]
+
+    iy = np.argsort(abs(r[:,1]))[-1]
+    ry = r[iy]
+    r = r[np.r_[0:iy,iy+1:len(r)]]
+
+    iz = np.argsort(abs(r[:,2]))[-1]
+    rz = r[iz]
+
+    return signed_volume(np.array((0,0,0)), rx, ry, rz)
+
+
+csv = 'data_both.csv'
+df = pd.read_csv(csv)
+triple_product = np.array([compute_invariant(ase.io.read(f'xyz-xtb/{i}.xyz.xtbopt.xyz')) for i in tqdm(df.id)])
+df['triple_product'] = triple_product
+assert np.allclose(triple_product.reshape((2,-1))[0], -triple_product.reshape((2,-1))[1], atol=1e-6)
+df.to_csv(csv, index=False)
--- data/rotation/dataloader_rotation.py
+++ data/rotation/dataloader_rotation.py
@@ -8,14 +8,15 @@ class Rotation(MolDataset):
                  target_column=None, geometry=None, graph_method=None,
                  noH=True, check=False):
 
-        self.version = 2  # INCREASE IF CHANGE THE DATA / DATALOADER / GRAPHS / ETC
-        self.csv_path='data/rotation/data.csv'
+        self.version = 3  # INCREASE IF CHANGE THE DATA / DATALOADER / GRAPHS / ETC
         self.processed_dir='data/rotation/processed/'
         self.smiles_column = None
         self.id_column = 'id'
-        self.default_parameters = dict(geometry='xtb', target_column='specific_rotation', graph_method='torchchem_v1')
+        self.default_parameters = dict(geometry='xtb', target_column='specific_rotation', graph_method='torchchem_v1',
+                                       _dl_extra_csv_name='data.csv')
         bad_indices = None
-        self.parameters = self.get_parameters(locals())
+        self.parameters = self.get_parameters(locals() | (extra_args if extra_args else {}))
+        self.csv_path = f'data/rotation/{self.parameters._dl_extra_csv_name}'  # data.csv or data_both.csv
 
         if self.parameters.geometry=='xtb':
             self.get_xyz_path = lambda idx: f'data/rotation/xyz-xtb/{idx}.xyz.xtbopt.xyz'
@@ -25,25 +26,3 @@ class Rotation(MolDataset):
                          bad_indices=bad_indices,
                          verbose=verbose)
 
-
-class RotationBoth(MolDataset):
-    def __init__(self, process=True, verbose=4,
-                 target_column=None, geometry=None, graph_method=None,
-                 noH=True, check=False):
-
-        self.version = 1  # INCREASE IF CHANGE THE DATA / DATALOADER / GRAPHS / ETC
-        self.csv_path='data/rotation/data_both.csv'
-        self.processed_dir='data/rotation/processed/'
-        self.smiles_column = None
-        self.id_column = 'id'
-        self.default_parameters = dict(geometry='xtb', target_column='specific_rotation', graph_method='torchchem_v1')
-        bad_indices = None
-        self.parameters = self.get_parameters(locals())
-
-        if self.parameters.geometry=='xtb':
-            self.get_xyz_path = lambda idx: f'data/rotation/xyz-xtb/{idx}.xyz.xtbopt.xyz'
-
-        super().__init__(process=process, noH=noH,
-                         check=check,
-                         bad_indices=bad_indices,
-                         verbose=verbose)
