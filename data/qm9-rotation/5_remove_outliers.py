4916d406a80920d10d88c14fdc7f911aea9d5083
Ksenia <ksenia.briling@epfl.ch>
2025-06-23 09:46:48 +0200

    Refactor 1fe24677d5

--- data/qm9-rotation/5_remove_outliers.py
+++ data/qm9-rotation/5_remove_outliers.py
@@ -8,12 +8,11 @@ df.drop(labels='023364', inplace=True)    # first excited state at 591.15nm
 df.drop(labels='071536', inplace=True)    # first excited state at 630.55nm
 
 def find_outliers(df, threshold):
-    slope, intercept, r_value, p_value, std_err = stats.linregress(df['x'], df['y'])
-    df['y_pred'] = intercept + slope * df['x']
+    k, b, r_value, _, _ = stats.linregress(df['x'], df['y'])
+    df['y_pred'] = b + k * df['x']
     df['dy'] = df['y'] - df['y_pred']
     df['outlier'] = np.abs(df['dy']) > threshold * df['dy'].std()
-    print(slope, intercept, r_value, p_value, std_err)
-    print(sum(df['outlier']))
+    print(f"{k= :4.2f}\t{b= :6.2f}\tR^2={np.sign(r_value)*r_value**2 :5.2f}\toutliers= {sum(df['outlier']):2}")
 
 def plot_outliers():
     import matplotlib.pyplot as plt
@@ -26,20 +25,19 @@ df['x'] = df['rot589']
 
 # for 589 and 633
 df['y'] = df['rot633']
-find_outliers(df, 20)
-df.drop(df[df['outlier']].index, inplace=True)
-find_outliers(df, 20)
-df.drop(df[df['outlier']].index, inplace=True)
-find_outliers(df, 40)
-df.drop(df[df['outlier']].index, inplace=True)
-find_outliers(df, 60)
+df['outlier'] = False
+for threshold in (20, 20, 40, 60):
+    df.drop(df[df['outlier']].index, inplace=True)
+    find_outliers(df, threshold)
 
+print()
 # for 355
 df['y'] = df['rot355']
 df['outlier'] = False
-for i in range(16):
+threshold = 20
+for i in range(12):
     df.drop(df[df['outlier']].index, inplace=True)
-    find_outliers(df, 20)
+    find_outliers(df, threshold)
 
 df.drop(labels=['x', 'y', 'y_pred', 'dy', 'outlier'], axis=1, inplace=True)
 df.to_csv('data.csv')
