1cba374838d9fb1b3a13385665e7bb09759d3ba5
Ksenia <ksenia.briling@epfl.ch>
2025-10-13 21:30:44 +0200

    Cleanup RX-OR dataset
    * center mols
    * rename

---
+++ data/reaxys-rotation/dataloader_rotation.py
@@ -0,0 +1,33 @@
+import numpy as np
+from process.dataloader import MolDataset
+
+
+class Rotation(MolDataset):
+    def __init__(self, process=True, verbose=4,
+                 extra_args=None,
+                 classification=False,
+                 target_column=None, geometry=None, graph_method=None,
+                 noH=True, check=False):
+
+        self.version = 4  # INCREASE IF CHANGE THE DATA / DATALOADER / GRAPHS / ETC
+        self.processed_dir='data/rotation/processed/'
+        self.smiles_column = None
+        self.id_column = 'id'
+        self.default_parameters = dict(geometry='xtb', target_column='specific_rotation', graph_method='torchchem_v1',
+                                       _dl_extra_csv_name='data.csv',
+                                       _dl_extra_bad_idx=None)
+
+        self.parameters = self.get_parameters(locals() | (extra_args if extra_args else {}))
+        self.csv_path = f'data/rotation/{self.parameters._dl_extra_csv_name}'   # data.csv or data_both.csv
+        bad_indices   = f'data/rotation/{self.parameters._dl_extra_bad_idx}' \
+                        if self.parameters._dl_extra_bad_idx else None          # negative_triple_idx.dat
+
+        if self.parameters.geometry=='xtb':
+            self.get_xyz_path = lambda idx: f'data/rotation/xyz-xtb/{idx}.xyz.xtbopt.xyz'
+
+        super().__init__(process=process, noH=noH,
+                         classification=classification,
+                         check=check,
+                         bad_indices=bad_indices,
+                         verbose=verbose)
+
