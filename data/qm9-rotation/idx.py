fbb04820dffadd81c915ebc530d4e74647bdd3a8
Ksenia <ksenia.briling@epfl.ch>
2025-12-08 17:11:00 +0100

    qm9-rotation splits

--- data/qm9-rotation/idx.py
+++ data/qm9-rotation/idx.py
@@ -1,9 +1,15 @@
 import numpy as np
-np.random.seed(666)
 
-N = 208740//2
-n = N//10
+for i in range(20):
 
-x = np.random.choice(N, size=n*2, replace=False)
-np.savetxt('idx_test.dat', np.concatenate((x[:n], x[:n]+N)), fmt='%d')
-np.savetxt('idx_val.dat', np.concatenate((x[n:], x[n:]+N)), fmt='%d')
+    if i>=10:
+        seed = 666 + (i - 10)
+    else:
+        seed = 123 + i
+
+    N = 205878//2
+    n = N//10
+
+    x = np.random.choice(N, size=n*2, replace=False)
+    np.savetxt(f'splits/idx_test.{i}.dat', np.concatenate((x[:n], x[:n]+N)), fmt='%d')
+    np.savetxt(f'splits/idx_val.{i}.dat', np.concatenate((x[n:], x[n:]+N)), fmt='%d')
