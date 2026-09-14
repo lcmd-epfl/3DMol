e499003dd4ed68b8b5569510af530658fc09229a
Ksenia <briling@mail.ru>
2026-09-14 17:13:11 +0200

    breaking change: Keep tanh in nonscaled but not in scaled

--- models/equimol.py
+++ models/equimol.py
@@ -175,15 +175,24 @@ class EquiMol(nn.Module):
             )
 
         if self.arch.startswith('both') or self.arch.startswith('pseudo'):
-            self.score_predictor_nodes_half_odd = nn.Sequential(
-                nn.Linear(self.n_s, 2 * self.n_s, bias=False),
-                nn.Tanh(),
-                nn.Dropout(dropout_p),
-                nn.Linear(2 * self.n_s, self.n_s, bias=False),
-                nn.Tanh(),
-                nn.Dropout(dropout_p),
-                nn.Linear(self.n_s, 1, bias=False)
-            )
+            if self.arch.endswith('_scaled'):
+                self.score_predictor_nodes_half_odd = nn.Sequential(
+                    nn.Linear(self.n_s, 2 * self.n_s, bias=False),
+                    nn.Dropout(dropout_p),
+                    nn.Linear(2 * self.n_s, self.n_s, bias=False),
+                    nn.Dropout(dropout_p),
+                    nn.Linear(self.n_s, 1, bias=False)
+                )
+            else:
+                self.score_predictor_nodes_half_odd = nn.Sequential(
+                    nn.Linear(self.n_s, 2 * self.n_s, bias=False),
+                    nn.Tanh(),
+                    nn.Dropout(dropout_p),
+                    nn.Linear(2 * self.n_s, self.n_s, bias=False),
+                    nn.Tanh(),
+                    nn.Dropout(dropout_p),
+                    nn.Linear(self.n_s, 1, bias=False)
+                )
 
         self.build_graph = BuildGraph(sh_irreps=self.sh_irreps, max_radius=self.max_radius,
                                       distance_emb_dim=self.distance_emb_dim, device=self.device)
